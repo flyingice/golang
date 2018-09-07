60c13b203cabe8eff96b4a205f3634987dc72a83
Yibin YANG <rainforest.vista@gmail.com>
2018-09-07 20:22:39 +0200

    Tour: Equivalent Binary Trees

---
+++ tour/exercise-equivalent-binary-trees/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/tour/tree"
+)
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func walk(t *tree.Tree, ch chan int) {
+	if t == nil {
+		return
+	}
+
+	walk(t.Left, ch)
+	ch <- t.Value
+	walk(t.Right, ch)
+}
+
+func Walk(t *tree.Tree, ch chan int) {
+	walk(t, ch)
+	close(ch)
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool {
+	ch1, ch2 := make(chan int), make(chan int)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+	for {
+		v1, ok1 := <-ch1
+		v2, ok2 := <-ch2
+		if !ok1 && !ok2 {
+			return true
+		} else if !ok1 || !ok2 || v1 != v2 {
+			return false
+		}
+	}
+}
+
+func main() {
+	if Same(tree.New(1), tree.New(1)) {
+		fmt.Println("OK")
+	} else {
+		fmt.Println("KO")
+	}
+}
