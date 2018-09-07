1a15f49ac633221fdbe19d99113156239ff5e928
Yibin YANG <rainforest.vista@gmail.com>
2018-09-07 19:55:40 +0200

    GOPL: looping in parallel pattern

---
+++ gopl/ch8/thumbnail/main.go
@@ -0,0 +1,150 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 234.
+
+// The package thumbnail produces thumbnail-size images from
+// larger images. Only JPEG images are currently supported.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"image"
+	"image/jpeg"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+)
+
+// Here is an exmpale using Channel and WaitGroup from GOPL chapter 8.
+// The code is a common and idiomatic pattrn for looping in parallel
+// when we don't know the number of iterations.
+
+// makeThumbnails6 makes thumbnails for each file received from the channel.
+// It returns the number of bytes occupied by the files it creates.
+func makeThumbnails6(filenames <-chan string) int64 {
+	sizes := make(chan int64)
+	var wg sync.WaitGroup // number of working goroutines
+	// input parameter is a channel and we don't know the number of sends in advance
+	for f := range filenames {
+		wg.Add(1) // increase the counter outside
+		// worker
+		go func(f string) {
+			defer wg.Done() // decrease the counter when the coroutine exits
+			thumb, err := ImageFile(f)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			info, _ := os.Stat(thumb) // OK to ignore error
+			sizes <- info.Size()
+		}(f)
+	}
+	// closer
+	// It must be executed in another goroutine since makeThumbnail6 is not able to
+	// close the channel itself being a receiver: If the wait() and close() are put
+	// in the same goroutine before the range over the channel, the program would be
+	// forever blocked since no receive can be done; If they are put after the range,
+	// a deadlock occurs and the wait() and close() are never executed.
+	go func() {
+		// the goroutine is blocked until the counter is down to zero, preventing closing
+		// the channel prematurely, which leads to panic when sender continues to write
+		// into an already closed channel.
+		wg.Wait()
+		// when the channel is closed, the receiver (range over sizes) is aware that
+		// there won't be any send anymore, the for loop thus exits safely.
+		close(sizes)
+	}()
+	var total int64
+	for size := range sizes {
+		total += size
+	}
+	return total
+}
+
+// Image returns a thumbnail-size version of src.
+func Image(src image.Image) image.Image {
+	// Compute thumbnail size, preserving aspect ratio.
+	xs := src.Bounds().Size().X
+	ys := src.Bounds().Size().Y
+	width, height := 128, 128
+	if aspect := float64(xs) / float64(ys); aspect < 1.0 {
+		width = int(128 * aspect) // portrait
+	} else {
+		height = int(128 / aspect) // landscape
+	}
+	xscale := float64(xs) / float64(width)
+	yscale := float64(ys) / float64(height)
+
+	dst := image.NewRGBA(image.Rect(0, 0, width, height))
+
+	// a very crude scaling algorithm
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			srcx := int(float64(x) * xscale)
+			srcy := int(float64(y) * yscale)
+			dst.Set(x, y, src.At(srcx, srcy))
+		}
+	}
+	return dst
+}
+
+// ImageStream reads an image from r and
+// writes a thumbnail-size version of it to w.
+func ImageStream(w io.Writer, r io.Reader) error {
+	src, _, err := image.Decode(r)
+	if err != nil {
+		return err
+	}
+	dst := Image(src)
+	return jpeg.Encode(w, dst, nil)
+}
+
+// ImageFile2 reads an image from infile and writes
+// a thumbnail-size version of it to outfile.
+func ImageFile2(outfile, infile string) (err error) {
+	in, err := os.Open(infile)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	out, err := os.Create(outfile)
+	if err != nil {
+		return err
+	}
+
+	if err := ImageStream(out, in); err != nil {
+		out.Close()
+		return fmt.Errorf("scaling %s to %s: %s", infile, outfile, err)
+	}
+	return out.Close()
+}
+
+// ImageFile reads an image from infile and writes
+// a thumbnail-size version of it in the same directory.
+// It returns the generated file name, e.g. "foo.thumb.jpeg".
+func ImageFile(infile string) (string, error) {
+	ext := filepath.Ext(infile) // e.g., ".jpg", ".JPEG"
+	outfile := strings.TrimSuffix(infile, ext) + ".thumb" + ext
+	return outfile, ImageFile2(outfile, infile)
+}
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		thumb, err := ImageFile(input.Text())
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		fmt.Println(thumb)
+	}
+	if err := input.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
