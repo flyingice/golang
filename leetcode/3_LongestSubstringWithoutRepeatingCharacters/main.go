8f7b65baba55b92e7dc29468d8caafbc2ca96794
Yibin YANG <rainforest.vista@gmail.com>
2018-09-08 17:25:28 +0200

    Algo 3. Longest Substring Without Repeating Characters https://leetcode.com/problems/longest-substring-without-repeating-characters/description/

--- leetcode/3_LongestSubstringWithoutRepeatingCharacters/main.go
+++ leetcode/3_LongestSubstringWithoutRepeatingCharacters/main.go
@@ -1,24 +1,24 @@
 package main
 
 func lengthOfLongestSubstring(s string) int {
-  var max_length, start int
-    m := make(map[byte]int)
-    for i, c := range []byte(s) {
-      if v, ok := m[c]; ok {
-        for j := start; j < v; j++ {
-          delete(m, s[j])
-        }
-        start = v + 1
-      } else {
-        if i - start + 1 > max_length {
-          max_length = i - start + 1
-        }
-      }
+	var maxLength, start int
+	m := make(map[byte]int)
+	for i, c := range []byte(s) {
+		if v, ok := m[c]; ok {
+			for j := start; j < v; j++ {
+				delete(m, s[j])
+			}
+			start = v + 1
+		} else {
+			if i-start+1 > maxLength {
+				maxLength = i - start + 1
+			}
+		}
 
-      m[c] = i
-    }
+		m[c] = i
+	}
 
-  return max_length
+	return maxLength
 }
 
 func main() {
